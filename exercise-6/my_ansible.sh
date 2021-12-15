e4f6c647704d1a87ed0c31a63fabb14d1ad00cea
Yanis Djebari <yanis.djebari@gmail.com>
2021-12-15 01:46:04 +0100

    tu est cringe

---
+++ exercise-6/my_ansible.sh
@@ -0,0 +1,4 @@
+mkdir -p documents/{gamez,images,work/{code,plannings}}
+touch -a meeting_notes.txt
+documents/{gamez/csgo.exe,work/plannings/{april.xlsx,february.xlsx,january.xlsx,march.xlsx}}
+exit 0
